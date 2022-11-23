8092d5866c99f295ea563a0fb1b3e384062e59d0
Dan Spakowicz <daniel.spakowicz@gmail.com>
2022-11-23 17:19:04 -0500

    figure and drake tweaks

--- manuscript/scripts/drake-figs/R/format_pathway_dat.R
+++ manuscript/scripts/drake-figs/R/format_pathway_dat.R
@@ -2,16 +2,16 @@ format_pathway_dat <- function(exprs, meta, generoles){
   ctcoms <- max(str_count(generoles$Genes, ",")) + 1
   lgenes.l <- generoles %>%
     separate(Genes, sep = ",", into = paste0("gene", 1:ctcoms)) %>%
-    gather(-Role, key = "gcount", value = "Gene") %>%
+    tidyr::gather(-Role, key = "gcount", value = "Gene") %>%
     filter(!is.na(Gene)) %>%
     dplyr::select(-gcount)
-  
+
   exprs.l <- exprs %>%
-    gather(-Gene, key = "sample", value = "counts")
-  
+    tidyr::gather(-Gene, key = "sample", value = "counts")
+
   boxin <- lgenes.l %>%
     left_join(exprs.l) %>%
     left_join(meta)
-  
+
   return(boxin)
 }
--- manuscript/scripts/drake-figs/R/nmds_neuts_pathways.R
+++ manuscript/scripts/drake-figs/R/nmds_neuts_pathways.R
@@ -1,49 +1,49 @@
 nmds_neuts_pathways <- function(davgroups, reactkey){
-  path.pvals <- lapply(names(davgroups), function(x)  pathwayDescriptionAssign(davgroups[[x]], reactkey) %>% 
+  path.pvals <- lapply(names(davgroups), function(x)  pathwayDescriptionAssign(davgroups[[x]], reactkey) %>%
                          dplyr::select(Category, Term, Description, PValue) %>%
                          mutate(source = x,
                                 FDR = p.adjust(PValue, method = "fdr"))) %>%
     bind_rows()
-  
-  
+
+
   path.bi.in <- path.pvals %>%
     dplyr::select(source, FDR, Description) %>%
     filter(!is.na(Description)) %>%
     filter(Description != "") %>%
     mutate(FDR = 1-FDR) %>%
     spread(key = "Description", value = 'FDR') %>%
-    gather(-source, key = "Description", value = "FDR") %>%
+    tidyr::gather(-source, key = "Description", value = "FDR") %>%
     mutate(FDR = ifelse(is.na(FDR), 0, FDR)) %>%
     spread(key = "Description", value = 'FDR')
-  
+
   path.novar <- lapply(path.bi.in[,-1], function(x) var(x))
   path.novar <- names(subset(path.novar, path.novar == 0))
-  
+
   path.nmds.in <- path.bi.in %>%
     dplyr::select(-path.novar)%>%
     column_to_rownames(var = "source")
-  
+
   set.seed(112358)
   path.nmds <- metaMDS(path.nmds.in)
-  
+
   path.nmds.points <- path.nmds$points %>%
     as.data.frame() %>%
     rownames_to_column(var = "pointname") %>%
     mutate(pointtype = "source")
-  
+
   path.nmds.species <- path.nmds$species %>%
     as.data.frame() %>%
     rownames_to_column(var = "pointname") %>%
     mutate(pointtype = "pathway")
-  
-  
+
+
   path.nmds.ggdat <- bind_rows(path.nmds.points, path.nmds.species) %>%
     mutate(pointlab = ifelse(pointtype == "source", pointname,
                              "")
            # ifelse(MDS1 > 0.5, pointname,
            #        ifelse(MDS2 > 1, pointname, "")))
     )
-  
+
   return(path.nmds.ggdat)
 }
 
@@ -54,20 +54,20 @@ pathwayDescriptionAssign <- function(davidoutput, reactkey){
     tmp.ls[[d]] <- davidoutput %>%
       filter(Category == d)
   }
-  
+
   tmp.ls$REACTOME_PATHWAY <- tmp.ls$REACTOME_PATHWAY %>%
     mutate(Term = gsub("(*.):.*","\\1", Term)) %>%
     left_join(reactkey)
-  
+
   tmp.ls$KEGG_PATHWAY <- tmp.ls$KEGG_PATHWAY %>%
     separate(Term, into = c("Term", "Description"), sep = ":")
-  
+
   tmp.ls$BIOCARTA <- tmp.ls$BIOCARTA %>%
     separate(Term, into = c("Term", "Description"), sep = ":")
-  
+
   tmp.ls$BBID <- tmp.ls$BBID %>%
     separate(Term, into = c("Term", "Description"), sep = ".")
-  
+
   tmp <- bind_rows(tmp.ls) %>%
     arrange(PValue)
   return(tmp)
