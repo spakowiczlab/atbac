5976fde707132b22d635259dd5a3c19c376e8a7d
hoyd44 <rhoyd44@gmail.com>
2023-12-18 14:28:49 -0500

    merge

---
+++ exploratory/scripts/R01-budget.R
@@ -0,0 +1,19 @@
+metaG <- 125
+
+m.samps.year <- 4
+
+years <- 5
+
+X16s <- 82
+
+x16.samps.year <- 24
+
+qPCR.yr <- 5000
+
+((metaG * m.samps.year) + (X16s * x16.samps.year) + qPCR.yr ) * years 
+
+cost.p.yr <- ((metaG * m.samps.year) + (X16s * x16.samps.year) + qPCR.yr )
+
+cost.p.yr
+
+cost.p.yr * years
